5cec00c3d6926093f62a707044de70e789cfdeaa
Mayukhdevan <mayukhdevansahu@yahoo.com>
2022-12-31 15:02:10 +0000

    Implemented GET, POST, PUT, DELETE APIs

--- index.js
+++ index.js
@@ -55,4 +55,106 @@ app.get("/books/:bookId/", async (request, response) => {
   response.send(bookObject);
 });
 
-//
+// Add Book API
+app.post("/books/", async (request, response) => {
+  const bookDetails = request.body;
+  const {
+    title,
+    authorId,
+    rating,
+    ratingCount,
+    reviewCount,
+    description,
+    pages,
+    dateOfPublication,
+    editionLanguage,
+    price,
+    onlineStores,
+  } = bookDetails;
+
+  const addBookQuery = `
+    INSERT INTO
+      book (title,author_id,rating,rating_count,review_count,description,pages,date_of_publication,edition_language,price,online_stores)
+    VALUES
+      (
+        '${title}',
+         ${authorId},
+         ${rating},
+         ${ratingCount},
+         ${reviewCount},
+        '${description}',
+         ${pages},
+        '${dateOfPublication}',
+        '${editionLanguage}',
+         ${price},
+        '${onlineStores}'
+      );`;
+
+  const dbResponse = await db.run(addBookQuery);
+  const bookId = dbResponse.lastID;
+  response.send({ bookId: bookId });
+});
+
+// Update Book API
+app.put("/books/:bookId/", async (request, response) => {
+  const { bookId } = request.params;
+  const bookDetails = request.body;
+  const {
+    title,
+    authorId,
+    rating,
+    ratingCount,
+    reviewCount,
+    description,
+    pages,
+    dateOfPublication,
+    editionLanguage,
+    price,
+    onlineStores,
+  } = bookDetails;
+
+  const updateBookQuery = `
+    UPDATE
+      book
+    SET
+      title='${title}',
+      author_id=${authorId},
+      rating=${rating},
+      rating_count=${ratingCount},
+      review_count=${reviewCount},
+      description='${description}',
+      pages=${pages},
+      date_of_publication='${dateOfPublication}',
+      edition_language='${editionLanguage}',
+      price= ${price},
+      online_stores='${onlineStores}'
+    WHERE
+      book_id = ${bookId};`;
+
+  await db.run(updateBookQuery);
+  response.send("Book Details Updated Successfully");
+});
+
+// Delete Book API
+app.delete("/books/:bookId/", async (request, response) => {
+  const { bookId } = request.params;
+  const deleteBookQuery = `
+    DELETE FROM
+      book
+    WHERE
+      book_id = ${bookId};`;
+  await db.run(deleteBookQuery);
+  response.send("Book Deleted Successfully");
+});
+
+// _______________Extra GET request _________________
+app.get("/authors/:authorId/books/", async (request, response) => {
+  const { authorId } = request.params;
+  const getAuthorBooksQuery = `
+    SELECT * FROM
+      book
+    WHERE
+      author_id = ${authorId};`;
+  const bookArray = await db.all(getAuthorBooksQuery);
+  response.send(bookArray);
+});
